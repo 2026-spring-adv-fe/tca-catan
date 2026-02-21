8d2b9c9ebd7ca53890a88201f1be36ba4c6bb590
furkansindar <fsindar@madisoncollege.edu>
2026-02-21 16:34:05 -0600

    starting fresh

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App 
-      initialCountValue={10}
-      // foo='furkan was here'
-    />
+    <App />
   </StrictMode>,
 )
